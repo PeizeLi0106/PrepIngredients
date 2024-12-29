0cbf479ab025533f9ece0b25c512c1932ef4a37e
Peize Li <peizeli@Peizes-Air.fritz.box>
2024-12-29 19:16:18 +0100

    added disk for images

--- index.js
+++ index.js
@@ -3,20 +3,31 @@ import bodyParser from "body-parser";
 import axios from "axios";
 import {WebSocketServer, WebSocket} from "ws";
 import http from "http";
-import compression from "compression";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// 食材顺序，分区，color-coded
-// sound cue
+// dotenv.config();
+// const port = process.env.PORT;
+// const bucketName = process.env.BUCKET_NAME;
+// const bucketRegion = process.env.BUCKET_REGION;
+// const accessKey = process.env.ACCESS_KEY;
+// const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
+// const s3 = new S3Client({
+//     credentials: {
+//         accessKeyId: accessKey,
+//         secretAccessKey: secretAccessKey,
+//     },
+//     region: bucketRegion,
+// });
 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
+const port = 3000;
 
 const server = http.createServer(app);
-const port = 3000;
 
 const wss = new WebSocketServer({server});
 
@@ -24,8 +35,6 @@ const wss = new WebSocketServer({server});
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.json());
 
-// Enable Gzip compression
-app.use(compression());
 
 // Serve CSS files with shorter caching
 app.use(
@@ -40,23 +49,46 @@ app.use(
 
 // Serve images with aggressive caching
 app.use(
-  "/images",
-  express.static(path.join(__dirname, "public/images"), {
-    maxAge: "1y", // Cache for 1 year
-    immutable: true, // Files won't change
-    setHeaders: (res) => {
-      res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
-    },
-  })
-);
+    "/images",
+    express.static("/var/www/images", {
+      maxAge: "1y", // Cache for 1 year
+      immutable: true, // Files won't change
+      setHeaders: (res) => {
+        res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
+      },
+    })
+  );
 
 
 
 // ingredients: a dict {ingredient_name: genre (meat or veg)}
 // dictionary remains constant; each time renders the lists
-// 肉: 1, 蔬菜: 2， 丸子: 3，海鲜: 4，菌类: 5，豆制品: 6，面制品: 7
+// 肉: 1, 蔬菜: 2，丸子: 3，海鲜: 4，菌类: 5，豆制品: 6，面制品: 7
 // 香菇 for meat side, 乌冬面 for veg side (exceptions)
-const ingredients_dict = {"牛肉": 1, "毛肚": 1, "菠菜": 2, "大白菜": 2, "羊肉": 1, "鱼丸": 3, "上海青": 2, "杏鲍菇": 5, "平菇": 5, "上海青": 2,"冻豆腐": 6,"香菇": 5,"茼蒿": 2,"菜心": 2,"金针菇": 5,"红薯": 2,"胡萝卜": 2,"南瓜": 2,"香菜": 2,"土豆": 2};
+const ingredients_dict = {"牛肉 Rindfleisch": 1, "五⾹⽜腱⼦ Gewürzte-Rindfleisch": 1, "鸡腿⾁ Gekochtes-Hähnchenfleisch": 1, "鸡胸⾁ Hühnerbrust": 1, 
+    "素⾁ Sojafasern": 1, "五⾹⽜蹄筋 Gewürzte-Rindersehne": 1, "⻧⼤肠 Schweinedarm": 1, "⽜⾁卷 Rindfleisch-Rolle": 1, "烤鸭 Entenfleisch": 1, 
+    "⽩百叶 Frische-Rindskutteln": 1, "⻩喉 Rinderhals": 1, "辣牛⾁ scharf-Rindfleisch": 1, "凤⽖ Hühnerfüße": 1, "⽺⾁卷 Lammfleisch-Rolle": 1, 
+    "⽜蛙 Ochsenfrosch": 1, "鸭珍 Entenmagen": 1, "⻧猪肚 Marinierte-Schweinemagen": 1, "⿊百叶 Schwarzer-Rindermagen": 1, "上海⻘ Pak-Choi": 2, "⼤⽩菜 Chinakohl": 2, 
+    "⽣菜 Blattsalat": 2, "菠菜 Spinat": 2, "茼蒿 Tong-Ho": 2, "莲藕 Lotuswurzel": 2, "⻄兰花 Brokkoli": 2, "⾹菜 Koriander": 2, "⽩萝⼘ Rettich": 2, 
+    "菊 苣 Chicorée": 2, "⼟⾖ Kartoffeln": 2, "胡萝⼘ Karotten": 2, "红薯 Süßkartoffel": 2, "⻩花菜 Getrocknete-Taglilien": 2, 
+    "⽵笋 Bambus": 2, "⼭药 Yamswurzel": 2, "南⽠ Kürbis": 2, "芋头 Taro": 2, "⾖芽 Sojasprossen": 2, "冬⽠ Wintermelone": 2, 
+    "平菇 Austernpilze": 2, "杏鲍菇 Königsausternpilze": 2, "⽟⽶尖 Mais-Sprossen": 2, "菜⼼ Choy-sum": 2, "⼟⽿其⽩卷⼼菜 Türkischer-Spitzkohl": 2, 
+    "菜花 Blumenkohl": 2, "油⻨菜 Chinesischer-Salat": 2, "荷兰⾖ Kaiserschoten": 2, "⻩⽠条 Gurke": 2, "⽟⽶ Mais": 2, "贡菜 Berggelee-Gemüse": 2, "芋仔 kleine-Taro-Knollen": 2,
+    "鲜⻥丸 Fischbällchen": 3, "午餐⾁ Frühstücksfleisch": 3, "墨⻥丸 Tintenfischbällchen": 3, "煎鸡蛋 Spiegelei": 3, "鸡⾁⼩⾹肠 Hähnchenwürstchen": 3, 
+    "虾丸 Garnelenbällchen": 3, "⽇本熏肠 Japanische-Räucherwurst": 3, "猫⽖虾丸 Katzenpfoten-Garnelenbällchen": 3, "糖⼼⻧蛋 Marinierte-Eier": 3, 
+    "⻥⾖腐 Fisch-Tofu": 3, "⽵轮烧 Chikuwa": 3, "福州⻥丸 Fuzhou-Fischbällchen": 3, "鸡⾁⾹肠 Hähnchenwurst": 3, "贡丸 Schweinefleisch-gefüllte-Bällchen": 3, 
+    "蟹棒 Surimi-Sticks": 3, "⽜⾁丸 Rindfleischbällchen": 3, "⽕腿肠 Schinkenwurst": 3,"海带 Seetang": 4, "章⻥ Oktopus": 4, 
+    "巴沙⻥ Pangasius-Fisch": 4, "⻘⼝⻉ Miesmuschel": 4, "⼤虾 Garnelen": 4, "虾仁 Geschälte-Garnelen": 4, "鲈⻥ Zanderfilet": 4, "鱿⻥花 Tintenfisch": 4, 
+    "⾦针菇 Enokipilze": 5, "鸿喜菇 Buchenpilz": 5, "⽺肚菌 Morcheln": 5, "⽊⽿ Mu-Err-Pilz": 5, "⾹菇 Shiitake-Pilz": 5, "腌蘑菇 eingelegte-Champignons": 5,
+    "鸭⾎⾖腐 Entenblut-Tofu": 6, "猪⾎⾖腐 Schweineblut-Tofu": 6, "腐⽵ Getrocknete-Tofustangen": 6, "⾖腐 Tofu": 6, "⾖腐丝 Tofu-Streifen": 6, 
+    "冻⾖腐 Gefrorener-Tofu": 6, "⾖腐⽪ Tofuhaut": 6, "⾖腐泡 Frittierter-Tofu": 6, "乌冬⾯ Udon-Nudeln": 7, "鸡⾁饺⼦ Hühnerfleisch-Maultaschen": 7, 
+    "素三鲜⽔饺 Vegetarische-Maultaschen": 7, "鸡⾁虾仁馄饨 Wan-Tan": 7, "⻩⾦蛋饺 Hähnchen-Ei-Maultaschen": 7, "功夫番茄⽜⾁饺⼦ Rindfeisch-Tomate-Maultaschen": 7, 
+    "⽶粉 Reisnudeln": 7, " ⽕锅川粉 Breite-Süßkatorffelnudeln": 7, "韩国素煎饺 Vegetarische-Koreanische-Mandu": 7, 
+    "鸡蛋⾯ Eiernudeln ": 7, "油条 Frittierte-Teigstangen": 7, "河粉 Reisbandnudeln": 7, "妈妈⾯ Mama-Nudeln": 7, "新鲜⾯条 Handgemachte-Nudeln": 7, 
+    "韩式南⽠⾯条 Koreanische-Kürbisnudeln": 7, "菠菜⾯条 Spinatnudeln": 7, "⽅便⾯ Instantnudeln": 7, "韩式拉⾯ Koreanische-Ramen": 7, "紫薯⾯条 Lila-Süßkartoffelnudeln": 7, 
+    "阳春⾯ Yamsnudeln": 7, " ⼑削⾯ Dao-Xiao-Mian": 7, "南⽠⾯条 Kürbisnudeln": 7, "荞⻨⾯ Buchweizennudeln": 7, "荞⻨⽅便⾯ Buchweizen-Instantnudeln": 7, "红薯粉丝 Süßkartoffeln-Glassnudeln": 7, 
+    "年糕 Reiskuchen": 7, "魔芋⾯ Konjaknudeln": 7, "魔芋结 Konjak-Knoten": 7, "韩国年糕条 Reiskuchen-Streifen ": 7, "韩国泡菜饺⼦ Koreanische-Kimchi-Teigtaschen": 7};
+
 var remain_lst = Object.keys(ingredients_dict);
 var shortage_dict = {}; // {name: urgency}
 var meat_side_dict = {}; // {name: urgency}
@@ -208,14 +240,16 @@ app.post("/remain/:name", async (req, res) => {
     }
     //console.log(urgent);
     const name = req.params.name;
+    //console.log(name);
     const genre = ingredients_dict[name]; // locate the genre so that we know to which kitchen we are sending
+    //console.log(genre);
     // remove the ingredient from remain_lst
     remain_lst = remain_lst.filter((remain) => {
         return remain !== name;
     });
     shortage_dict[name] = urgent;
     // update the meat end if it is a meat; otherwise update the veg end
-    if (genre === 1 || genre === 3 || genre === 4 || genre === 6 || (genre === 7 && name !== "乌冬面") || (genre === 5 && name === "香菇")){
+    if (genre === 1 || (genre === 2 && (name === "玉米" || name === "贡菜" || name === "芋仔")) || genre === 3 || genre === 4 || (genre === 5 && (name === "香菇" || name === "木耳" || name === "腌蘑菇")) || genre === 6 || (genre === 7 && name !== "乌冬面")){
         try {
             const response = await axios.post(`http://localhost:${port}/addMeat`, {name: name, urgent: urgent});
         }catch (error){
@@ -241,7 +275,7 @@ app.post("/shortage/:name", async (req, res)=>{
     delete shortage_dict[name];
     remain_lst = [...remain_lst, name]; // add the ingredient back to the remain_lst
     // update the meat end if it is a meat; otherwise update the veg end
-    if (genre === 1 || genre === 3 || genre === 4 || genre === 6 || (genre === 7 && name !== "乌冬面") || (genre === 5 && name === "香菇")){
+    if (genre === 1 || (genre === 2 && (name === "玉米" || name === "贡菜" || name === "芋仔")) || genre === 3 || genre === 4 || (genre === 5 && (name === "香菇" || name === "木耳" || name === "腌蘑菇")) || genre === 6 || (genre === 7 && name !== "乌冬面")){
         try {
             const response = await axios.post(`http://localhost:${port}/removeMeat`, {name: name});
         }catch (error){
