7db763e0df40ce7f6d99db16ad71f6531af4cecf
Peize Li <peizeli@Peizes-Air.fritz.box>
2024-12-30 15:21:35 +0100

    fixed image filenames issue

--- index.js
+++ index.js
@@ -50,16 +50,16 @@ app.use(
 );
 
 // Serve images with aggressive caching
-app.use(
-    "/images",
-    express.static("/var/www/images", {
-      maxAge: "1y", // Cache for 1 year
-      immutable: true, // Files won't change
-      setHeaders: (res) => {
-        res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
-      },
-    })
-  );
+// app.use(
+//     "/images",
+//     express.static("/var/www/images", {
+//       maxAge: "1y", // Cache for 1 year
+//       immutable: true, // Files won't change
+//       setHeaders: (res) => {
+//         res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
+//       },
+//     })
+//   );
 
 
 
@@ -67,30 +67,29 @@ app.use(
 // dictionary remains constant; each time renders the lists
 // 肉: 1, 蔬菜: 2，丸子: 3，海鲜: 4，菌类: 5，豆制品: 6，面制品: 7
 // 香菇 for meat side, 乌冬面 for veg side (exceptions)
-const ingredients_dict = {"牛肉 Rindfleisch": 1, "五⾹⽜腱⼦ Gewürzte-Rindfleisch": 1, "鸡腿⾁ Gekochtes-Hähnchenfleisch": 1, "鸡胸⾁ Hühnerbrust": 1, 
-    "素⾁ Sojafasern": 1, "五⾹⽜蹄筋 Gewürzte-Rindersehne": 1, "⻧⼤肠 Schweinedarm": 1, "⽜⾁卷 Rindfleisch-Rolle": 1, "烤鸭 Entenfleisch": 1, 
-    "⽩百叶 Frische-Rindskutteln": 1, "⻩喉 Rinderhals": 1, "辣牛⾁ scharf-Rindfleisch": 1, "凤⽖ Hühnerfüße": 1, "⽺⾁卷 Lammfleisch-Rolle": 1, 
-    "⽜蛙 Ochsenfrosch": 1, "鸭珍 Entenmagen": 1, "⻧猪肚 Marinierte-Schweinemagen": 1, "⿊百叶 Schwarzer-Rindermagen": 1, "上海⻘ Pak-Choi": 2, "⼤⽩菜 Chinakohl": 2, 
-    "⽣菜 Blattsalat": 2, "菠菜 Spinat": 2, "茼蒿 Tong-Ho": 2, "莲藕 Lotuswurzel": 2, "⻄兰花 Brokkoli": 2, "⾹菜 Koriander": 2, "⽩萝⼘ Rettich": 2, 
-    "菊苣 Chicorée": 2, "⼟⾖ Kartoffeln": 2, "胡萝⼘ Karotten": 2, "红薯 Süßkartoffel": 2, "⻩花菜 Getrocknete-Taglilien": 2, 
-    "⽵笋 Bambus": 2, "⼭药 Yamswurzel": 2, "南⽠ Kürbis": 2, "芋头 Taro": 2, "⾖芽 Sojasprossen": 2, "冬⽠ Wintermelone": 2, 
-    "平菇 Austernpilze": 2, "杏鲍菇 Königsausternpilze": 2, "⽟⽶尖 Mais-Sprossen": 2, "菜⼼ Choy-sum": 2, "⼟⽿其⽩卷⼼菜 Türkischer-Spitzkohl": 2, 
-    "菜花 Blumenkohl": 2, "油⻨菜 Chinesischer-Salat": 2, "荷兰⾖ Kaiserschoten": 2, "⻩⽠条 Gurke": 2, "⽟⽶ Mais": 2, "贡菜 Berggelee-Gemüse": 2, "芋仔 kleine-Taro-Knollen": 2,
-    "鲜⻥丸 Fischbällchen": 3, "午餐⾁ Frühstücksfleisch": 3, "墨⻥丸 Tintenfischbällchen": 3, "煎鸡蛋 Spiegelei": 3, "鸡⾁⼩⾹肠 Hähnchenwürstchen": 3, 
-    "虾丸 Garnelenbällchen": 3, "⽇本熏肠 Japanische-Räucherwurst": 3, "猫⽖虾丸 Katzenpfoten-Garnelenbällchen": 3, "糖⼼⻧蛋 Marinierte-Eier": 3, 
-    "⻥⾖腐 Fisch-Tofu": 3, "⽵轮烧 Chikuwa": 3, "福州⻥丸 Fuzhou-Fischbällchen": 3, "鸡⾁⾹肠 Hähnchenwurst": 3, "贡丸 Schweinefleisch-gefüllte-Bällchen": 3, 
-    "蟹棒 Surimi-Sticks": 3, "⽜⾁丸 Rindfleischbällchen": 3, "⽕腿肠 Schinkenwurst": 3,"海带 Seetang": 4, "章⻥ Oktopus": 4, 
-    "巴沙⻥ Pangasius-Fisch": 4, "⻘⼝⻉ Miesmuschel": 4, "⼤虾 Garnelen": 4, "虾仁 Geschälte-Garnelen": 4, "鲈⻥ Zanderfilet": 4, "鱿⻥花 Tintenfisch": 4, 
-    "⾦针菇 Enokipilze": 5, "鸿喜菇 Buchenpilz": 5, "⽺肚菌 Morcheln": 5, "⽊⽿ Mu-Err-Pilz": 5, "⾹菇 Shiitake-Pilz": 5, "腌蘑菇 eingelegte-Champignons": 5,
-    "鸭⾎⾖腐 Entenblut-Tofu": 6, "猪⾎⾖腐 Schweineblut-Tofu": 6, "腐⽵ Getrocknete-Tofustangen": 6, "⾖腐 Tofu": 6, "⾖腐丝 Tofu-Streifen": 6, 
-    "冻⾖腐 Gefrorener-Tofu": 6, "⾖腐⽪ Tofuhaut": 6, "⾖腐泡 Frittierter-Tofu": 6, "乌冬⾯ Udon-Nudeln": 7, "鸡⾁饺⼦ Hühnerfleisch-Maultaschen": 7, 
-    "素三鲜⽔饺 Vegetarische-Maultaschen": 7, "鸡⾁虾仁馄饨 Wan-Tan": 7, "⻩⾦蛋饺 Hähnchen-Ei-Maultaschen": 7, "功夫番茄⽜⾁饺⼦ Rindfeisch-Tomate-Maultaschen": 7, 
-    "⽶粉 Reisnudeln": 7, " ⽕锅川粉 Breite-Süßkatorffelnudeln": 7, "韩国素煎饺 Vegetarische-Koreanische-Mandu": 7, 
-    "鸡蛋⾯ Eiernudeln ": 7, "油条 Frittierte-Teigstangen": 7, "河粉 Reisbandnudeln": 7, "妈妈⾯ Mama-Nudeln": 7, "新鲜⾯条 Handgemachte-Nudeln": 7, 
-    "韩式南⽠⾯条 Koreanische-Kürbisnudeln": 7, "菠菜⾯条 Spinatnudeln": 7, "⽅便⾯ Instantnudeln": 7, "韩式拉⾯ Koreanische-Ramen": 7, "紫薯⾯条 Lila-Süßkartoffelnudeln": 7, 
-    "阳春⾯ Yamsnudeln": 7, " ⼑削⾯ Dao-Xiao-Mian": 7, "南⽠⾯条 Kürbisnudeln": 7, "荞⻨⾯ Buchweizennudeln": 7, "荞⻨⽅便⾯ Buchweizen-Instantnudeln": 7, "红薯粉丝 Süßkartoffeln-Glassnudeln": 7, 
-    "年糕 Reiskuchen": 7, "魔芋⾯ Konjaknudeln": 7, "魔芋结 Konjak-Knoten": 7, "韩国年糕条 Reiskuchen-Streifen ": 7, "韩国泡菜饺⼦ Koreanische-Kimchi-Teigtaschen": 7};
-
+const ingredients_dict = {"牛肉 Rindfleisch": 1, "五香牛腱子 Gewürzte-Rindfleisch": 1, "鸡腿肉 Gekochtes-Hähnchenfleisch": 1, "鸡胸肉 Hühnerbrust": 1, 
+    "素肉 Sojafasern": 1, "五香牛蹄筋 Gewürzte-Rindersehne": 1, "卤大肠 Schweinedarm": 1, "牛肉卷 Rindfleisch-Rolle": 1, "烤鸭 Entenfleisch": 1, 
+    "白百叶 Frische-Rindskutteln": 1, "黄喉 Rinderhals": 1, "辣牛肉 scharf-Rindfleisch": 1, "凤爪 Hühnerfüße": 1, "羊肉卷 Lammfleisch-Rolle": 1, 
+    "牛蛙 Ochsenfrosch": 1, "鸭胗 Entenmagen": 1, "卤猪肚 Marinierte-Schweinemagen": 1, "黑百叶 Schwarzer-Rindermagen": 1, "上海青 Pak-Choi": 2, "大白菜 Chinakohl": 2, 
+    "生菜 Blattsalat": 2, "菠菜 Spinat": 2, "茼蒿 Tong-Ho": 2, "莲藕 Lotuswurzel": 2, "西兰花 Brokkoli": 2, "香菜 Koriander": 2, "白萝卜 Rettich": 2, 
+    "菊苣 Chicorée": 2, "土豆 Kartoffeln": 2, "胡萝卜 Karotten": 2, "红薯 Süßkartoffel": 2, "黄花菜 Getrocknete-Taglilien": 2, 
+    "竹笋 Bambus": 2, "山药 Yamswurzel": 2, "南瓜 Kürbis": 2, "芋头 Taro": 2, "豆芽 Sojasprossen": 2, "冬瓜 Wintermelone": 2, 
+    "平菇 Austernpilze": 2, "杏鲍菇 Königsausternpilze": 2, "玉米尖 Mais-Sprossen": 2, "菜心 Choy-sum": 2, "土耳其白卷心菜 Türkischer-Spitzkohl": 2, 
+    "菜花 Blumenkohl": 2, "油麦菜 Chinesischer-Salat": 2, "荷兰豆 Kaiserschoten": 2, "黄瓜条 Gurke": 2, "玉米 Mais": 2, "贡菜 Berggelee-Gemüse": 2, "芋仔 kleine-Taro-Knollen": 2,
+    "鲜鱼丸 Fischbällchen": 3, "午餐肉 Frühstücksfleisch": 3, "墨鱼丸 Tintenfischbällchen": 3, "煎鸡蛋 Spiegelei": 3, "鸡肉小香肠 Hähnchenwürstchen": 3, 
+    "虾丸 Garnelenbällchen": 3, "日本熏肠 Japanische-Räucherwurst": 3, "猫爪虾丸 Katzenpfoten-Garnelenbällchen": 3, "溏心卤蛋 Marinierte-Eier": 3, 
+    "鱼豆腐 Fisch-Tofu": 3, "竹轮烧 Chikuwa": 3, "福州鱼丸 Fuzhou-Fischbällchen": 3, "鸡肉香肠 Hähnchenwurst": 3, "贡丸 Schweinefleisch-gefüllte-Bällchen": 3, 
+    "蟹棒 Surimi-Sticks": 3, "牛肉丸 Rindfleischbällchen": 3, "火腿肠 Schinkenwurst": 3,"海带 Seetang": 4, "章鱼 Oktopus": 4, 
+    "巴沙鱼 Pangasius-Fisch": 4, "青口贝 Miesmuschel": 4, "大虾 Garnelen": 4, "虾仁 Geschälte-Garnelen": 4, "鲈鱼 Zanderfilet": 4, "鱿鱼花 Tintenfisch": 4, 
+    "金针菇 Enokipilze": 5, "鸿喜菇 Buchenpilz": 5, "羊肚菌 Morcheln": 5, "木耳 Mu-Err-Pilz": 5, "香菇 Shiitake-Pilz": 5, "腌蘑菇 eingelegte-Champignons": 5,
+    "鸭血豆腐 Entenblut-Tofu": 6, "猪血豆腐 Schweineblut-Tofu": 6, "腐竹 Getrocknete-Tofustangen": 6, "鲜豆腐 Tofu": 6, "豆腐丝 Tofu-Streifen": 6, 
+    "冻豆腐 Gefrorener-Tofu": 6, "豆腐皮 Tofuhaut": 6, "豆腐泡 Frittierter-Tofu": 6, "乌冬面 Udon-Nudeln": 7, "鸡肉饺子 Hühnerfleisch-Maultaschen": 7, 
+    "素三鲜水饺 Vegetarische-Maultaschen": 7, "鸡肉虾仁馄饨 Wan-Tan": 7, "黄金蛋饺 Hähnchen-Ei-Maultaschen": 7, "功夫番茄牛肉饺子 Rindfeisch-Tomate-Maultaschen": 7, 
+    "米粉 Reisnudeln": 7, "火锅川粉 Breite-Süßkatorffelnudeln": 7, "韩式素煎饺 Vegetarische-Koreanische-Mandu": 7, 
+    "鸡蛋面 Eiernudeln ": 7, "油条 Frittierte-Teigstangen": 7, "河粉 Reisbandnudeln": 7, "妈妈面 Mama-Nudeln": 7, "新鲜面条 Handgemachte-Nudeln": 7, 
+    "韩式南瓜面条 Koreanische-Kürbisnudeln": 7, "菠菜面条 Spinatnudeln": 7, "方便面 Instantnudeln": 7, "韩式面条 Koreanische-Ramen": 7, "紫薯面条 Lila-Süßkartoffelnudeln": 7, 
+    "阳春面 Yamsnudeln": 7, "刀削面 Dao-Xiao-Mian": 7, "南瓜面条 Kürbisnudeln": 7, "荞麦面 Buchweizennudeln": 7, "荞麦方便面 Buchweizen-Instantnudeln": 7, "红薯粉丝 Süßkartoffeln-Glassnudeln": 7, 
+    "年糕 Reiskuchen": 7, "魔芋面 Konjaknudeln": 7, "魔芋结 Konjak-Knoten": 7, "韩国年糕条 Reiskuchen-Streifen ": 7, "韩国泡菜饺子 Koreanische-Kimchi-Teigtaschen": 7};
 var remain_lst = Object.keys(ingredients_dict);
 var shortage_dict = {}; // {name: urgency}
 var meat_side_dict = {}; // {name: urgency}
